02b3c377e99737be6e24d59860d37d20647255ea
Santi-quintero <squintero@godigital.com.co>
2022-10-24 16:20:28 -0500

    update objeto para todos los personajes de todos los episodios

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -1,200 +1,226 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Character, CharacterList, Episode, Origin} from 'src/app/models/character';
+import { Character, CharacterList, Origin } from 'src/app/models/character';
 import { CharacterService } from 'src/app/service/character.service';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-
   characters!: Character[];
   details!: Character;
   charactersEpisodes!: Character[];
   origin!: Origin[];
-  charactersList: CharacterList[]=[];
-
+  charactersList: CharacterList[] = [];
 
   constructor(
     private characterService: CharacterService,
-    private modal:NgbModal
+    private modal: NgbModal
   ) {
-    this.characters=[]
-    
-   }
+    this.characters = [];
+  }
 
   ngOnInit(): void {
     this.cargarCharacters();
     // this.getCharactersEpisode();
   }
 
-  cargarCharacters(){
+  cargarCharacters() {
     this.characterService.list().subscribe(
-      data => {
-        this.characters = data.results
+      (data) => {
+        this.characters = data.results;
       },
-      err => {
-        console.log(err.message)
+      (err) => {
+        console.log(err.message);
       }
-    )
+    );
   }
 
-  searchCharacters(name: string){
-     this.characterService.search(name).subscribe(
-       data=>{
-         this.characters = data.results
-       },
-       err =>{
-         console.log(err.message)
-       }
-     )
-
+  searchCharacters(name: string) {
+    this.characterService.search(name).subscribe(
+      (data) => {
+        this.characters = data.results;
+      },
+      (err) => {
+        console.log(err.message);
+      }
+    );
   }
-  detailCharacter(contenido: any, id: number){
-    this.modal.open(contenido,{size:'xl'});
+  detailCharacter(contenido: any, id: number) {
+    this.modal.open(contenido, { size: 'xl' });
     this.characterService.detail(id).subscribe(
-      data=>{
-        this.details = data
-      },err=>{
-        console.log(err.message)
+      (data) => {
+        this.details = data;
+      },
+      (err) => {
+        console.log(err.message);
       }
-    )
-
-    this.getCharactersEpisode(id)
+    );
+    this.getCharactersEpisodes(id);
+    //this.getCharactersEpisode(id)
   }
-  getCharactersEpisode(id: number){
-    let characterEpisodes = {}
-    this.characterService.detailCharacterEpisode(id).subscribe(
-      data=>{
-         fetch(data)
-         .then((res) => {
+  getCharactersEpisode(id: number) {
+    let characterEpisodes = {};
+    this.characterService.detailCharacterEpisode(id).subscribe((data) => {
+      fetch(data)
+        .then((res) => {
           return res.json();
-          })
-         .then((res)=>{
+        })
+        .then((res) => {
           let promises = [];
           for (const character of res.characters) {
             promises.push(this.getApiData(character));
           }
           return Promise.all(promises);
-          })
-          .then((data)=>{
-            console.log(data)
-            for (let i = 0; i < data.length; i++) {
-              characterEpisodes[data[i].id] = { id: data[i].id, image: data[i].image };
-            }
-            let origins = [];
-            for (const i of data) {
-              if (i.origin.url === "") {
-                origins.push({
-                  name: "Desconocido",
-                  url: "https://rickandmortyapi.com/api/location/6"
-                });
-              } else {
-                origins.push(i.origin);
-              }
-            }
-            return origins;
-          })
-          .then((data) => {
-            let promises = [];
-            for (const i of data) {
-              promises.push(i.url);
-            }
-            return Promise.all(promises);
-          })
-          .then((data) => {
-            let promises = [];
-            for (const i of data) {
-              promises.push(this.getApiData(i));
-            }
-            return Promise.all(promises);
-          })
-          .then((data) => {
-           let promises = []
-           for (const key in characterEpisodes) {
-             promises.push(characterEpisodes[key])
-           }
-            for (let index = 0; index < data.length; index++) {
-              promises[index].name = data[index].name;
-              promises[index].type = data[index].type;
-              promises[index].dimension = data[index].dimension;
-        }
-            this.charactersList= promises;
-            console.log(this.charactersList);
-          });
-  
-      }
-    )
-  }
-
-  
-
-   getCharactersEpisodes(id: number){
-     this.characterService.detailCharacterEpisode(id).subscribe(
-       data=>{
-          fetch(data)
-          .then((res) => {
-           return res.json();
-           })
-          .then((res)=>{
-           let promises = [];
-           for (const character of res.characters) {
-             promises.push(this.getApiData(character));
-           }
-           return Promise.all(promises);
-           })
-           .then((data)=>{
-             this.charactersEpisodes=data
-             let origins = [];
-             for (const i of data) {
-             // if (i.origin.url != "") {
-               //   origins.push(i.origin);
-               // }
-               if (i.origin.url === "") {
-                 origins.push({"name": "Earth (C-137)","url": "https://rickandmortyapi.com/api/location/1"});
-            }else{
+        })
+        .then((data) => {
+          console.log(data);
+          for (let i = 0; i < data.length; i++) {
+            characterEpisodes[data[i].id] = {
+              id: data[i].id,
+              image: data[i].image,
+            };
+          }
+          let origins = [];
+          for (const i of data) {
+            if (i.origin.url === '') {
+              origins.push({
+                name: 'Desconocido',
+                url: 'https://rickandmortyapi.com/api/location/6',
+              });
+            } else {
               origins.push(i.origin);
             }
-             }
-             return origins;
-           })
-           .then((origins) => {
-             //console.log(origins);
-             let promises=[];
-             for (const i of origins) {
-               promises.push(i.url);
-             }
-             return Promise.all(promises)
-           })
-           .then((origins)=>{
-             let promises=[];
-             for (const i of origins) {
-               promises.push(this.getApiData(i));
-             }
-             return Promise.all(promises)
-           })
-           .then((promises)=>{
-             this.origin = promises;
-             console.log(promises)
-           })
-  
+          }
+          return origins;
+        })
+        .then((data) => {
+          let promises = [];
+          for (const i of data) {
+            promises.push(i.url);
+          }
+          return Promise.all(promises);
+        })
+        .then((data) => {
+          let promises = [];
+          for (const i of data) {
+            promises.push(this.getApiData(i));
+          }
+          return Promise.all(promises);
+        })
+        .then((data) => {
+          let promises = [];
+          for (const key in characterEpisodes) {
+            promises.push(characterEpisodes[key]);
+          }
+          for (let index = 0; index < data.length; index++) {
+            promises[index].name = data[index].name;
+            promises[index].type = data[index].type;
+            promises[index].dimension = data[index].dimension;
+          }
+          this.charactersList = promises;
+          console.log(this.charactersList);
+        });
+    });
+  }
+
+  getCharactersEpisodes(id: number) {
+    this.characterService.episodes(id).subscribe((data) => {
+      let characterEpisodes = {};
+      let promises = [];
+      for (const i of data.episode) {
+        promises.push(this.getApiData(i));
+      }
+      Promise.all(promises)
+      .then((data) => {
+        let promises3 = [];
+        for (const character of data) {
+          promises3.push(character.characters);
+        }
+        return Promise.all(promises3)
+      })
+      .then((res)=>{
+       // console.log(res)
+       let arrayconcat = [];
+       for (const key in res) {
+        arrayconcat = arrayconcat.concat(res[key]);
        }
-     )
-   }
+       return Promise.all(arrayconcat);
+      })
+      .then((data) => {
+        let promises = [];
+        for (const i of data) {
+          promises.push(this.getApiData(i));
+        }
+        return Promise.all(promises);
+      })
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          characterEpisodes[data[i].id] = { id: data[i].id, image: data[i].image };
+        }
+        let origins = [];
+        for (const i of data) {
+          if (i.origin.url === "") {
+            origins.push({
+              name: "Desconocido",
+              url: "https://rickandmortyapi.com/api/location/6"
+            });
+          } else {
+            origins.push(i.origin);
+          }
+        }
+        return origins;
+      })
+      .then((data) => {
+        let promises = [];
+        for (const i of data) {
+          promises.push(i.url);
+        }
+        return Promise.all(promises);
+      })
+      .then((data) => {
+        let promises = [];
+        for (const i of data) {
+          promises.push(this.getApiData(i));
+        }
+        return Promise.all(promises);
+      })
+      .then((data) => {
+        //console.log(data)
+        let promises = [];
+        for (const key in characterEpisodes) {
+          promises.push(characterEpisodes[key]);
+        }
   
+        for (var i = 0; i < data.length; i++) {
+          for (const key in promises) {
+            promises[key].name = data[key].name;
+            promises[key].type = data[key].type;
+            promises[key].dimension = data[key].dimension;
+          }
+        }
+        this.charactersList = promises;
+      })
+      .then((res)=>{
+       
+        // console.log(res)
+        // console.log(characterEpisodes)
+       })
+    });
+  }
 
   getApiData = (url: string) => {
-     return new Promise((resolve, reject) => {
-       fetch(url)
-         .then((data) => {
-           return data.json();
-         })
-         .then(resolve)
-         .catch((error) => {
-         console.log(url, error);
-         });
-     });
-   };
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((data) => {
+          return data.json();
+        })
+        .then(resolve)
+        .catch((error) => {
+          console.log(url, error);
+        });
+    });
+  };
 }
